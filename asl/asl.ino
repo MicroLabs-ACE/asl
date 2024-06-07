562b5b3bb92e317646ca8db1b98c19c89fb115c7
Victor Momodu <victoramomodu@gmail.com>
2024-06-07 22:54:52 +0100

    Initial commit

---
+++ asl/asl.ino
@@ -0,0 +1,94 @@
+#include <TensorFlowLite_ESP32.h>
+
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/system_setup.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+
+#include "model.h"
+
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+TfLiteTensor* output = nullptr;
+constexpr int kTensorArenaSize = 3 * 1024;
+uint8_t tensor_arena[kTensorArenaSize];
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
+
+  model = tflite::GetModel(model_tflite);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Model provided is schema version %d not equal to supported version %d.", model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+
+  static tflite::AllOpsResolver resolver;
+
+  static tflite::MicroInterpreter static_interpreter(model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
+  interpreter = &static_interpreter;
+
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
+
+  input = interpreter->input(0);
+  output = interpreter->output(0);
+}
+
+int splitString(String data, char delimiter, String* parts) {
+  int delimiterIndex = data.indexOf(delimiter);
+
+  if (delimiterIndex == -1) return 0;
+  else {
+    parts[0] = data.substring(0, delimiterIndex);
+    parts[1] = data.substring(delimiterIndex + 1);
+
+    return 2;
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    String parts[2];
+
+    int count = splitString(data, ',', parts);
+    if (count == 2) {
+      Serial.print(parts[0]);
+      Serial.print(", ");
+      Serial.println(parts[1]);
+    } else {
+      Serial.println("Invalid input. Please provide two comma-separated values.");
+    }
+
+    input->data.f[0] = parts[0].toFloat();
+    input->data.f[1] = parts[1].toFloat();
+
+    TfLiteStatus invoke_status = interpreter->Invoke();
+    if (invoke_status != kTfLiteOk) {
+      Serial.println("Error occured in invoking interpreter.");
+      return;
+    }
+
+    Serial.print(output->data.f[0]);
+    Serial.print(" ");
+    Serial.print(output->data.f[1]);
+    Serial.print(" ");
+    Serial.print(output->data.f[2]);
+    Serial.print(" ");
+    Serial.print(output->data.f[3]);
+    Serial.println();
+    Serial.println();
+  }
+}
---
+++ train.py
@@ -0,0 +1,81 @@
+import os
+import random
+
+random.seed(42)
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+
+# Generate data
+top_left_quadrant_range = ((0, 99), (0, 99))
+top_right_quadrant_range = ((100, 199), (0, 100))
+bottom_left_quadrant_range = ((0, 99), (100, 199))
+bottom_right_quadrant_range = ((100, 199), (100, 199))
+
+X = []
+y = []
+
+for _ in range(4000):
+    ranges = (
+        top_left_quadrant_range,
+        top_right_quadrant_range,
+        bottom_left_quadrant_range,
+        bottom_right_quadrant_range,
+    )
+    choice_ranges = random.choice(ranges)
+    horizontal_range, vertical_range = choice_ranges
+    horizontal = random.randint(*horizontal_range)
+    vertical = random.randint(*vertical_range)
+    target = [0 for _ in range(4)]
+    target_index = ranges.index(choice_ranges)
+    target[target_index] = 1
+
+    X.append([horizontal, vertical])
+    y.append(target)
+
+X = np.array(X)
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+)
+
+# Train model
+model = tf.keras.Sequential(
+    [
+        tf.keras.Input(shape=(2,)),
+        tf.keras.layers.Dense(8, activation="relu"),
+        tf.keras.layers.Dense(8, activation="relu"),
+        tf.keras.layers.Dense(8, activation="relu"),
+        tf.keras.layers.Dense(4, activation="softmax"),  # 3 classes for Iris dataset
+    ]
+)
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+model.fit(X_train, y_train, epochs=100, batch_size=16, verbose=1)
+loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+print(f"Test Accuracy: {accuracy}")
+
+y_pred = model.predict(X_test)
+predicted_classes = np.argmax(y_pred, axis=1)
+true_classes = np.argmax(y_test, axis=1)
+
+for i in range(10):
+    print(f"True: {true_classes[i]}, Predicted: {predicted_classes[i]}")
+
+model.export("model")
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_saved_model("model")
+tflite_model = converter.convert()
+
+# Save the model
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
